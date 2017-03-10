07bacf055bc9159a1f8fbdac7bcbd533e405532e
Justin Menga <justin.menga@gmail.com>
2017-03-10 22:34:16 +1300

    Log to stdout

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -4,5 +4,8 @@ set -e
 # Run confd to render config file(s)
 confd -onetime -backend env
 
+# Grant permissions to /dev/stdout for spawned chrony process
+chown chrony:chrony /dev/stdout
+
 # Run application
 exec "$@"
