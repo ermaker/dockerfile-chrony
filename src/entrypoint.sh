f071dbd35dbdaf2584208e1817baddae94f18c0b
Minwoo Lee <ermaker@gmail.com>
2018-12-28 17:34:17 +0900

    Remove pid file

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -7,5 +7,8 @@ confd -onetime -backend env
 # Grant permissions to /dev/stdout for spawned chrony process
 chown chrony:chrony /dev/stdout
 
+# Remove pid file if exists
+rm -f /var/run/chronyd.pid
+
 # Run application
 exec "$@"
